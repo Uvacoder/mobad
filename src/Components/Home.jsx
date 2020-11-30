21ce56b69bd3042d415e66d808b92e2915a8cdb0
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-11-30 18:03:17 -0500

    components outline

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ class Home extends React.Component {
     render() {
         console.log(this.props)
         return (
-            <div>
+            <div className="home">
             <h3>Home stuff goes here</h3>
             </div>
         );
---
+++ src/Components/Music.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Music = () => {
+
+    return (
+
+        <div className="music">
+            <p> Music </p>
+        </div>
+    )
+}
+export default Music;
