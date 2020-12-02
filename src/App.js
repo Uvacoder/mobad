13c0e7629badf38040d3e61a1bafd4eeda16026c
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-12-02 11:54:13 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import HomeContainer from './Containers/HomeContainer';
 import AboutContainer from './Containers/AboutContainer';
 import Navbar from './Components/Navbar'
 import Grid from 'animated-grid-lines';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 class App extends Component {
     render() {
-  return (
-    <div className="App">
-    <Navbar />
-    <Grid/>
-    <HomeContainer/>
-    <footer>© {new Date().toISOString().substring(0, 4)} Mobâd</footer>
-    </div>
-  );
-}
+        return (
+            <div className="App">
+                <Navbar />
+                <Grid />
+                <Switch>
+                <HomeContainer />
+                </Switch>
+                <footer>© {new Date().toISOString().substring(0, 4)} Mobâd</footer>
+            </div>
+        );
+    }
 }
 
 export default App;
